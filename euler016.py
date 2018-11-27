869dd182c1c73c60540ed1e61f5ecfec3731a07c
Manulainen <manu@localhost.localdomain>
2018-11-27 21:06:25 +0100

    [mlage] Add template to 016

--- euler016.py
+++ euler016.py
@@ -6,10 +6,14 @@
         2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
         What is the sum of the digits of the number 2^1000?
 """
-
 # =============================================================================
-# [resuelto] 1366
-# [comprobado]
+# (upper left label is the current label)
+#
+# SOLUTION: 1366
+#
+# STATUS: [ok]
+# VERIFIED: [ok]
+# EXECUTION TIME: [ok]
 # =============================================================================
 
 def euler16(power):
